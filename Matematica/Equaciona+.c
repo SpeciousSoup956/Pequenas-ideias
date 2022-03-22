1ffc05b1855fb7be7e760741beacc1d412577290
Mariane Andrade <86253405+SpeciousSoup956@users.noreply.github.com>
2022-03-22 10:17:05 -0300

    Adicionada a pasta matematica
    adicionado a pasta: equaciona+.c

---
+++ Matematica/Equaciona+.c
@@ -0,0 +1,47 @@
+/******************************************************************************
+OBJETIVO: RESOLVER EQUAÇÕES
+NOME: -----
+DATA DE CRIAÇÃO: 22.03.2022
+STATUS: ANDAMENTO...
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+
+
+int topo (void)
+{
+    //cabeçalho
+  printf("**********************************");
+  printf("\nBem vindo (a) ao Equaciona+");
+  printf("\n**********************************\n");
+  
+}
+
+int escolha (void)
+{
+    //variaveis
+    int valor = 0;
+  
+    //desenvolvimento
+  printf("Qual tipo de equação você quer resolver?\n");
+  printf("\n1. Equação de 1° Grau.");
+  printf("\n2. Equação de 2° Grau.");  
+  printf ("\n\n>>> Digite o número da operação desejada: ");
+  scanf("%d", &valor);
+  
+}
+
+int main()
+{
+   /* setlocale(LC_ALL, "Portuguese");*/
+   //variaveis
+   
+   //desenvolvimento
+    topo();
+    printf ("\n >> ");
+    escolha ();
+
+    return 0;
+}
